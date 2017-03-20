1cfe4d8d89cbeba74b42bffc0f1ab7cdeeb53c4f
Ben Greaves <bsgreaves@gmail.com>
2017-03-20 17:01:37 -0500

    Done, ready for feedback

--- dom.js
+++ dom.js
@@ -1,54 +1,22 @@
 var donateBtn = document.getElementById("donateBtn");
-donateBtn.addEventListener("click", gatherData);
+var inputName = document.getElementById("name")
+var inputEmail = document.getElementById("email");
+var inputAmount = document.getElementById("amount");
+donateBtn.addEventListener("click", addNewDonor);
 
-function gatherData() {
+function addNewDonor() {
     let newDonor = {};
-    newDonor.name = document.getElementById("name").value;
-    newDonor.email = document.getElementById("email").value;
-    newDonor.amount = document.getElementById("amount").value;
+    newDonor.name = inputName.value;
+    newDonor.email = inputEmail.value;
+    newDonor.amount = inputAmount.value;
     if (document.getElementById("oneTime").checked === true) {
         newDonor.method = "One-time"
     } else {
         newDonor.method = "Per-lap"
     }
     Donors.addDonor(newDonor);
+    Donors.displayDonors();
+    inputName.value = "";
+    inputEmail.value = "";
+    inputAmount.value = "";
 }
-
-//Everything below is purely so that I can get some data in the table on page load
-//It's a copy-paste from the IIFE
-
-var dummyData = [{
-    name: "Ben Greaves",
-    email: "bsgreaves@gmail.com",
-    amount: "$20.00",
-    method: "One-time"
-}, {
-    name: "Billy Zane",
-    email: "zany@gmail.com",
-    amount: "$3.00",
-    method: "Per-lap"
-}, {
-    name: "George Dubya",
-    email: "georgieboy@gmail.com",
-    amount: "$150.00",
-    method: "One-time"
-}];
-
-function dummyDisplay(donors) {
-    let donorStr = "";
-    let donorDisplay = document.getElementById("donorDisplay");
-    donorStr += `<table class="table table-striped">
-						<th>
-							<td class="bold">Amount</td>
-							<td class="bold">Type</td>
-							<td class="bold">Email</td>
-						</th>`
-    for (let i = 0; i < donors.length; i++) {
-        currDonor = donors[i];
-        donorStr += `<tr><td class="bold">${currDonor.name}</td><td>${currDonor.amount}</td><td>${currDonor.method}</td><td>${currDonor.email}</td></tr>`
-    }
-    donorStr += `</table>`
-    donorDisplay.innerHTML = donorStr;
-}
-
-dummyDisplay(dummyData);
